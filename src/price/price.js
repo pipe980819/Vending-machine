e5027a91da5db0dd22450423ef30821deb763681
Julian Andres Sanchez <juliansanchez0319@gmail.com>
2020-09-25 18:21:06 -0500

    Add endpoint

--- src/price/price.js
+++ src/price/price.js
@@ -1,11 +1,11 @@
 const prices = {
-  "Papas margaritas": 2300,
-  "Chiclitos": 800,
+  "B1": { name: "Papas margaritas", price: 2300, },
+  "B2": { name: "Choclitos", price: 800, },
 }
 exports.productPrice = (product) => {
-  return prices[product];
+  return prices[product].price;
 }
 
 exports.exchange = (product, pay) => {
-  return pay - prices[product];
+  return pay - prices[product].price;
 }
