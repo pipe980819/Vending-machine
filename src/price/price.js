a4bcaf0b8923316da88b35b1a8ce264531773881
Julian Andres Sanchez <juliansanchez0319@gmail.com>
2020-09-25 19:31:22 -0500

    Add eslint action

--- src/price/price.js
+++ src/price/price.js
@@ -1,11 +1,12 @@
 const prices = {
-  "B1": { name: "Papas margaritas", price: 2300, },
-  "B2": { name: "Choclitos", price: 800, },
-}
+  'B1': { name: 'Papas margaritas', price: 2300, },
+  'B2': { name: 'Choclitos', price: 800, },
+};
+
 exports.productPrice = (product) => {
   return prices[product].price;
-}
+};
 
 exports.exchange = (product, pay) => {
   return pay - prices[product].price;
-}
+};
