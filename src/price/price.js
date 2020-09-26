38ee52c8f1efdcba2ddc2530bfaa49792196e11e
Julian Andres Sanchez <juliansanchez0319@gmail.com>
2020-09-26 08:29:36 -0500

    Addd code format error

--- src/price/price.js
+++ src/price/price.js
@@ -1,12 +1,12 @@
 const prices = {
   'B1': { name: 'Papas margaritas', price: 2300, },
-  'B2': { name: 'Choclitos', price: 800, },
+  'B2': { name: "Choclitos", price: 800, },
 };
 
 exports.productPrice = (product) => {
-  return prices[product].price;
+  return prices[product].price
 };
 
 exports.exchange = (product, pay) => {
-  return pay - prices[product].price;
+  return pay - prices[product].price
 };
